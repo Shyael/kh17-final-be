0c6a0df1c6256799c29469e99ae4d9afb3a7aa7b
tookwak5-sudo <tookwak5@gmail.com>
2026-08-18 20:02:56 +0900

    파이널 프로젝트 시작

---
+++ khedu/src/main/java/com/kh/khedu/KheduApplication.java
@@ -0,0 +1,13 @@
+package com.kh.khedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KheduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KheduApplication.class, args);
+	}
+
+}
