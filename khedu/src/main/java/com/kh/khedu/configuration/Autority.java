60d32532456286b4378a34275713d8d6786b371a
tookwak5-sudo <tookwak5@gmail.com>
2026-08-24 03:02:38 +0900

    filterchain 구현중

---
+++ khedu/src/main/java/com/kh/khedu/configuration/Autority.java
@@ -0,0 +1,8 @@
+package com.kh.khedu.configuration;
+
+public final class Autority { 
+	private Autority() {}
+	
+	public static final String STUDENT_READ = "학생조회";
+	public static final String STUDENT_DELETE = "학생삭제";
+}
