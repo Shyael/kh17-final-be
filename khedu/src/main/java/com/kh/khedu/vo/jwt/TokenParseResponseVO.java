5b5c77a05161a77f867342c1ca568b7759c571f0
tookwak5-sudo <tookwak5@gmail.com>
2026-08-27 19:29:18 +0900

    response도 수정

--- khedu/src/main/java/com/kh/khedu/vo/jwt/TokenParseResponseVO.java
+++ khedu/src/main/java/com/kh/khedu/vo/jwt/TokenParseResponseVO.java
@@ -12,5 +12,5 @@ public class TokenParseResponseVO {
 	private int accountNo;
 	private String accountId;	
 	private String accountType; // 직원, 학생, 학부모
-	private List<Integer> roleNos;//권한 
+	private List<String> roleNames;//권한 
 }
