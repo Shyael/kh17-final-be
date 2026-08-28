bb68436cd328cb2e66d3ed573269dda406111ab4
tookwak5-sudo <tookwak5@gmail.com>
2026-08-28 17:41:31 +0900

    직원 정보 등록 구현중

---
+++ khedu/src/main/java/com/kh/khedu/dao/StudentDao.java
@@ -0,0 +1,5 @@
+package com.kh.khedu.dao;
+
+public interface StudentDao {
+
+}
---
+++ khedu/src/main/java/com/kh/khedu/dao/StudentDaoMybatis.java
@@ -0,0 +1,9 @@
+package com.kh.khedu.dao;
+
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class StudentDaoMybatis implements StudentDao {
+
+}
