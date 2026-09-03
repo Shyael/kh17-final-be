8ccc10ac223de0b9d9072599b71cf65525c14bab
tookwak5-sudo <tookwak5@gmail.com>
2026-09-03 10:41:20 +0900

    관리자 - 직원 목록 상세 구현중

--- khedu/src/main/java/com/kh/khedu/controller/AdminEmployeeRestController.java
+++ khedu/src/main/java/com/kh/khedu/controller/AdminEmployeeRestController.java
@@ -1,17 +1,29 @@
 package com.kh.khedu.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kh.khedu.service.EmployeeService;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = "관리자 직원 정보 관리 서비스")
+@Tag(name = "관리자 직원 정보 관리")
 @RestController
 @RequestMapping("/api/admin/employee")
 public class AdminEmployeeRestController {
 	
-	//직원 목록
+	@Autowired
+	private EmployeeService employeeService;
 	
+	//직원 목록
+//	@GetMapping("/")
+//	public List<AdminEmployeeDetailVO> list() {
+//		return employeeService.findAllEmployees();
+//	}
 	
 	//직원 상세
 	
