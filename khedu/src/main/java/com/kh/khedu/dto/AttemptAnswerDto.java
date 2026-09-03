c8fc080dbbc01ade8bdec8d18f42a78baa273b64
wjdduddnd0108-ops <wjdduddnd0108@gmail.com>
2026-09-03 14:02:47 +0900

    시험전체 DTO 기초 mapper작성완료

--- khedu/src/main/java/com/kh/khedu/dto/AttemptAnswerDto.java
+++ khedu/src/main/java/com/kh/khedu/dto/AttemptAnswerDto.java
@@ -1,5 +1,20 @@
 package com.kh.khedu.dto;
 
-public class AttemptAnswerDto {
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Schema(name = "시험 답안 DTO")
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class AttemptAnswerDto {
+    private int attemptNo;
+    private int questionNo;
+    // 선택한 보기 번호
+    // 미응답이면 null 가능
+    private Integer optionNo;
+    // 정답 여부 Y / N
+    // 채점 전이면 null 가능
+    private String isCorrect;
 }
