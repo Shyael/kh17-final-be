4a93ba07e0edbb5294e4eb7b8aadc3b540d2544b
Shyael <seokkid@gmail.com>
2026-09-21 14:04:29 +0900

    배포

--- khedu/src/main/java/com/kh/khedu/dao/StudentDaoMybatis.java
+++ khedu/src/main/java/com/kh/khedu/dao/StudentDaoMybatis.java
@@ -105,6 +105,7 @@ public class StudentDaoMybatis implements StudentDao {
 	@Override
 	public boolean approveAccount(int studentNo) {  
 		return sqlSession.update("mapper.student.approveAccount", studentNo) > 0;
+		
 	}
 
 	@Override
